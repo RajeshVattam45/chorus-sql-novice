51e1c1ead1ab34b28d913a325e4c254e072a86cc
RajeshVattam45 <vattamrajesh45@gmail.com>
2024-10-29 19:02:02 +0530

    SQL views and index's

---
+++ views_indexes/indexs/Index.sql
@@ -0,0 +1,16 @@
+-- Query to create index table.
+CREATE TABLE index_table (
+    id INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    role VARCHAR(30) NOT NULL,
+    type VARCHAR(30) NOT NULL
+);
+
+-- Query to create index to columns in a table.
+CREATE INDEX indexs ON index_table (id, name);
+
+-- Show all indexes on 'index_table'.
+EXEC sp_helpindex 'index_table';
+
+-- Query to delete the index
+DROP INDEX indexs ON index_table;
---
+++ views_indexes/views/Views.sql
@@ -0,0 +1,71 @@
+-- Note: create the users table if it does not already exist.
+CREATE TABLE users (
+    id INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+    user_name VARCHAR(30) NOT NULL,
+    user_email VARCHAR(30) NOT NULL,
+    user_number INT NOT NULL,
+    user_role VARCHAR(30) NOT NULL
+);
+
+-- Create a view to display basic user information.
+CREATE VIEW user_info AS
+SELECT
+    id,
+    user_name,
+    user_email
+FROM
+    users;
+
+-- Query to delete the view.
+DROP VIEW user_info;
+
+-- Query to display data from user_info view.
+SELECT
+    *
+FROM
+    user_info;
+
+CREATE VIEW user_info_details AS
+SELECT
+    *
+FROM
+    users
+WHERE
+    id = 5;
+
+-- Query to retrieve data from the updated user_info_details view.
+SELECT
+    *
+FROM
+    user_info_details;
+
+-- Query to delete the view.
+DROP VIEW user_info_details;
+
+-- View to Hide Sensitive Information.
+CREATE VIEW display_limited_details AS
+SELECT
+    id,
+    user_name
+FROM
+    user;
+
+SELECT
+    *
+FROM
+    display_limited_details;
+
+-- Create a view to display all information for the user with user_name 'rajesh'.
+CREATE VIEW [User Rajesh] AS
+SELECT
+    *
+FROM
+    users
+WHERE
+    user_name = 'rajesh';
+
+-- Query to retrieve data from the 'User Rajesh' view.
+SELECT
+    *
+FROM
+    [User Rajesh];
